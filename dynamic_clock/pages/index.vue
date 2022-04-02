cacc79ad5ae9f56f144fc34e4b6bbaf6f8f4701d
Vatsal <chauhanvatsal55@gmail.com>
2022-04-02 18:41:57 +0530

    feat: add clock component

--- dynamic_clock/pages/index.vue
+++ dynamic_clock/pages/index.vue
@@ -1,8 +1,11 @@
 <template>
+  <progress-clock />
 </template>
 
 <script>
+import ProgressClock from '~/components/ProgressClock.vue'
 export default {
+  components: { ProgressClock },
   name: 'IndexPage',
 }
 </script>
